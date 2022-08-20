ad84459fbb5f8feb7d1cd8469b38486995c3518f
Kyabetsu8931Ya31 <yuuki.20020217@gmail.com>
2022-08-20 22:19:22 +0900

    update robots.txt

--- SatisfactionConfiguration/urls.py
+++ SatisfactionConfiguration/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from .setview import robots
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/devicesearch/',include('devicesearch.urls')),
+    path('robots.txt',robots)
 ]
