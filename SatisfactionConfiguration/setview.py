d38aafffbd60c7060cda3817df25769b9665ac41
Kyabetsu8931Ya31 <yuuki.20020217@gmail.com>
2022-08-21 20:51:27 +0900

    update robots

--- SatisfactionConfiguration/setview.py
+++ SatisfactionConfiguration/setview.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def robots(request):
-    return render(request,'static/robots.txt')
+    return render(request=request,template_name='robots.txt',content_type='text/plain')
